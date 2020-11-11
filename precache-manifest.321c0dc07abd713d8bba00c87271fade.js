7c7741f27beb21fc57469f2c493fc3c338852c24
Lakshita Agarwal <lakshitaagarwal23@gmial.com>
2020-11-11 14:01:47 +0530

    covid-tracker

---
+++ precache-manifest.321c0dc07abd713d8bba00c87271fade.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09a9e95b81fdd4d588dd369a2aadb034",
+    "url": "/index.html"
+  },
+  {
+    "revision": "05e29fcc914c5e4349a9",
+    "url": "/static/css/main.c4d37f22.chunk.css"
+  },
+  {
+    "revision": "d0c953326947e84e588d",
+    "url": "/static/js/2.ad6089e9.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/static/js/2.ad6089e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05e29fcc914c5e4349a9",
+    "url": "/static/js/main.167b990b.chunk.js"
+  },
+  {
+    "revision": "717a8abd0f249375b28a",
+    "url": "/static/js/runtime-main.0dea4190.js"
+  },
+  {
+    "revision": "24fbea10e27f7277d5c54afd48ba2797",
+    "url": "/static/media/logo.24fbea10.png"
+  }
+]);
